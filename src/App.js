bd11f46bb3150ea903d4a67da9f013d638ec8705
francisco javier florez ramirez <franciscojflorezr12@gmail.com>
2023-08-28 21:29:32 -0500

    cards and trello master

--- src/App.js
+++ src/App.js
@@ -2,10 +2,65 @@
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        hello world
-      </h1>
+    <div className="trello-master">
+      <nav className="navbar app">App bar</nav>
+      <nav className="navbar board">Board bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png"></img>
+              Desing & Reaserch
+            </li>
+            <li>second</li>
+            <li>second</li>
+            <li>second</li>
+            <li>second</li>
+            <li>second</li>
+            <li>second</li>
+            <li>second</li>
+            <li>second</li>
+            <li>second</li>
+            <li>second</li>
+            <li>second</li>
+            <li>second</li>
+            <li>second</li>
+            <li>second</li>
+            <li>second</li>
+            <li>second</li>
+            <li>second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png"></img>
+              Desing & Reaserch
+            </li>
+            <li>second</li>
+            <li>second</li>
+            <li>second</li>
+            <li>second</li>
+            <li>second</li>
+            <li>second</li>
+            <li>second</li>
+            <li>second</li>
+            <li>second</li>
+            <li>second</li>
+            <li>second</li>
+            <li>second</li>
+            <li>second</li>
+            <li>second</li>
+            <li>second</li>
+            <li>second</li>
+            <li>second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
