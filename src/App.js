8375538eb24f9356411fec7e274425d46655bc5d
francisco javier florez ramirez <franciscojflorezr12@gmail.com>
2023-09-13 00:49:10 -0500

    Columnas y tareas

--- src/App.js
+++ src/App.js
@@ -1,67 +1,12 @@
 import AppBar from "./components/AppBar/AppBar";
 import BoardBar from "./components/BoardBar/BoardBar";
-
+import BoardContent from "./components/BoardContent/BoardContent";
 function App() {
   return (
     <div className="trello-master">
       <AppBar/>
       <BoardBar/>
-      <div className="board-columns">
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png"></img>
-              Desing & Reaserch
-            </li>
-            <li>second</li>
-            <li>second</li>
-            <li>second</li>
-            <li>second</li>
-            <li>second</li>
-            <li>second</li>
-            <li>second</li>
-            <li>second</li>
-            <li>second</li>
-            <li>second</li>
-            <li>second</li>
-            <li>second</li>
-            <li>second</li>
-            <li>second</li>
-            <li>second</li>
-            <li>second</li>
-            <li>second</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png"></img>
-              Desing & Reaserch
-            </li>
-            <li>second</li>
-            <li>second</li>
-            <li>second</li>
-            <li>second</li>
-            <li>second</li>
-            <li>second</li>
-            <li>second</li>
-            <li>second</li>
-            <li>second</li>
-            <li>second</li>
-            <li>second</li>
-            <li>second</li>
-            <li>second</li>
-            <li>second</li>
-            <li>second</li>
-            <li>second</li>
-            <li>second</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-      </div>
+      <BoardContent/>
     </div>
   );
 }
---
+++ src/components/Task/Task.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Task.scss";
+const Task = () => {
+  return (
+    <>
+      <li className="task-item">
+        <img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png"></img>
+        Desing & Reaserch
+      </li>
+    </>
+  );
+};
+
+export default Task;
